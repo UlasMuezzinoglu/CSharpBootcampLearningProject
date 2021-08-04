661e6a5fad18e792cf4dfdc745cc3e1383359b67
UlasMuezzinoglu <batan_gem1@outlook.com>
2021-08-05 00:17:56 +0300

    Cross Origin Configured

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -44,6 +44,7 @@ namespace WebAPI
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
+            services.AddCors();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -79,6 +80,7 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
